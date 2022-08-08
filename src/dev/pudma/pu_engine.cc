1353f21b645d48883854b2f16d0968cb4ff91ac7
soarstill <soarstill@gmail>
2022-08-09 00:54:42 +0900

    pudma: PuEngine
    Change-Id: I080f69d222a01a02b10f68e49ffa8cf9af709319

--- src/dev/pudma/pu_engine.cc
+++ src/dev/pudma/pu_engine.cc
@@ -36,20 +36,55 @@
 namespace gem5
 {
 
+
 PuEngine::PuEngine(const PuEngineParams &params) :
     SimObject(params),
-    // This is a C++ lambda. When the event is triggered, it will call the
-    // processEvent() function. (this must be captured)
-    event([this]{ processEvent(); }, name() + ".event"),
+    instPort(params.name + ".inst_port", this),
+    dataPort(params.name + ".data_port", this),
+    memPort(params.name + ".mem_side", this),
+    blocked(false),
     pucore(params.pucore_object),
-    // Note: This is not needed as you can *always* reference this->name()
-    myName(params.name),
+    event([this] {processEvent();}, name() + ".event"),
     latency(params.time_to_wait),
     timesLeft(params.number_of_fires)
+
 {
-    DPRINTF(PUDMA, "Created the PuEngine object\n");
-    panic_if(!pucore, "PuEngine must have a non-null PuCore object");
+    DPRINTF(PUDMA,"Created the PuEngine object\n");
+    panic_if(!pucore,"PuEngine must have a non-null PuCore object");
 }
+
+Port &
+PuEngine::getPort(const std::string &if_name, PortID idx)
+{
+    panic_if(idx != InvalidPortID, "This object doesn't support vector ports");
+
+    // This is the name from the Python SimObject declaration (SimpleMemobj.py)
+    if (if_name == "mem_side") return memPort;
+    else if (if_name == "inst_port") return instPort;
+    else if (if_name == "data_port") return dataPort;
+    else {
+        return SimObject::getPort(if_name, idx);
+    }
+}
+/**
+ * Return the address ranges this memobj is responsible for. Just use the
+ * same as the next upper level of the hierarchy.
+ *
+ * @return the address ranges this memobj is responsible for
+ */
+AddrRangeList
+PuEngine::getAddrRanges() const
+{
+    return memPort.getAddrRanges();
+}
+
+void
+PuEngine::sendRangeChange()
+{
+    instPort.sendRangeChange();
+    dataPort.sendRangeChange();
+}
+
 
 void
 PuEngine::startup()
@@ -61,16 +96,184 @@ PuEngine::startup()
 void
 PuEngine::processEvent()
 {
-    timesLeft--;
-    DPRINTF(PUDMA, "PuEngine: Processing the event! %d left\n",
+    DPRINTF(PUDMA,"PuEngine: Processing the event! %d left\n",
                           timesLeft);
-
-    if (timesLeft <= 0) {
-        DPRINTF(PUDMA, "Done firing!\n");
-        pucore->compute(myName);
+    if (--timesLeft <= 0) {
+        "Done %d firing! \n", timesLeft);
+        pucore->compute("SONG ");
     } else {
         schedule(event, curTick() + latency);
     }
 }
+
+bool PuEngine::handleRequest(PacketPtr pkt)
+{
+    // just forward to the Mem side with blocked flag set
+    if (blocked) {
+        return false;
+    }
+    if (pkt->req->isInstFetch()) {
+        DPRINTF(PUDMA,"PuEngine:: Got ifetch request from addr %#x\n",
+                pkt->getAddr());
+    } else {
+        DPRINTF(PUDMA,"PuEngine:: Got data request from addr %#x\n",
+                pkt->getAddr());
+    }
+
+    blocked = true;
+    memPort.sendReqPacket(pkt);
+
+    return true;
+}
+
+
+bool PuEngine::handleResponse(PacketPtr pkt)
+{
+    assert(blocked);
+    DPRINTF(PUDMA,"PuEngine:: Get response from addr %#x\n",
+            pkt->getAddr());
+
+    blocked = false;
+
+    if (pkt->req->isInstFetch()) {
+        instPort.sendRespPacket(pkt);
+    } else {
+        dataPort.sendRespPacket(pkt);
+    }
+
+    return true;
+}
+
+void PuEngine::handleFunctional(PacketPtr pkt)
+{
+    memPort.sendFunctional(pkt);
+}
+
+/////////////////////////////////////////////////////////////////////
+//// PuEngine::CPUSidePort implementation ///////
+
+/**
+ * @brief get address range list of this device owned this port
+ * @param void None
+ *
+ */
+AddrRangeList
+PuEngine::CPUSidePort::getAddrRanges() const
+{
+    DPRINTF(PUDMA,"Sending new ranges\n");
+
+    return owner->getAddrRanges();
+}
+
+
+void
+PuEngine::CPUSidePort::recvFunctional(PacketPtr pkt)
+{
+    // just forward to the owner
+    return owner->handleFunctional(pkt);
+}
+
+
+bool
+PuEngine::CPUSidePort::recvTimingReq(PacketPtr pkt)
+{
+    // just forward to the owner
+    if ( !owner->handleRequest(pkt)){
+        needRetry = true;
+        return false;
+    } else {
+        return true;
+    }
+
+}
+
+void
+PuEngine::CPUSidePort::recvRespRetry()
+{
+    assert(blockedPacket != nullptr);
+
+    PacketPtr pkt = blockedPacket;
+    blockedPacket = nullptr;
+
+    sendRespPacket(pkt);
+
+}
+
+/**
+ *
+ * @brief send a response packet to the cpu_side port
+ * @param - packet to send
+ *
+ *
+*/
+void
+PuEngine::CPUSidePort::sendRespPacket(PacketPtr pkt)
+{
+   // Note: This flow control is very simple since the memobj is blocking.
+    panic_if(blockedPacket != nullptr,
+        "Owner never try to respond if blocked");
+
+    if (!sendTimingResp(pkt)) {
+        blockedPacket = pkt;
+    }
+}
+
+/**
+ * Send a retry to the peer port only if it is needed. This is called
+ * from the PuEngine whenever it is unblocked.
+ */
+void
+PuEngine::CPUSidePort::trySendRetry()
+{
+    assert(blockedPacket != nullptr);
+
+    PacketPtr pkt = blockedPacket;
+    blockedPacket = nullptr;
+
+    sendRespPacket(pkt);
+}
+
+/////////////////////////////////////////////////////////////////////
+//// PuEngine::MemSidePort implementation ///////
+void
+PuEngine::MemSidePort::recvRangeChange()
+{
+    // just forward the responsibility to the owner: notify the CPU sides
+    owner->sendRangeChange();
+}
+
+bool
+PuEngine::MemSidePort::recvTimingResp(PacketPtr pkt)
+{
+    return owner->handleResponse(pkt);
+}
+
+void
+PuEngine::MemSidePort::recvReqRetry()
+{
+    assert(blockedPacket != nullptr);
+
+    PacketPtr pkt = blockedPacket;
+    blockedPacket = nullptr;
+
+    sendReqPacket(pkt);
+}
+
+
+/**
+ * @brief send a request packet pkt to devices connected to the memside_port
+ *
+ * @param pkt
+ */
+void
+PuEngine::MemSidePort::sendReqPacket(PacketPtr pkt)
+{
+    panic_if(blockedPacket != nullptr, "never try to send if blocked");
+
+    if (!sendTimingReq(pkt)) {
+        blockedPacket = pkt;
+    }
+}
+
 
 } // namespace gem5
--- src/dev/pudma/pu_engine.hh
+++ src/dev/pudma/pu_engine.hh
@@ -31,7 +31,9 @@
 
 #include <string>
 
+#include "dev/pudma/pu_core.hh"
 #include "dev/pudma/pu_engine.hh"
+#include "mem/port.hh"
 #include "params/PuEngine.hh"
 #include "sim/sim_object.hh"
 
@@ -40,26 +42,180 @@ namespace gem5
 
 class PuEngine : public SimObject
 {
-  private:
     /**
-     * Example function to execute on an event trigger
+     * @brief Ports class definition for PuEngine py class
+     * input: inst_port, data_port
+     * output: mem_side
      */
-    void processEvent();
+    class CPUSidePort : public ResponsePort
+    {
+      private:
+        PuEngine *owner;
+        bool needRetry;
+        PacketPtr blockedPacket;
 
-    /// An event that wraps the above function
+      public:
+        CPUSidePort(const std::string &name, PuEngine *owner) :
+          ResponsePort(name,owner), needRetry(false), blockedPacket(nullptr)
+        {}
+
+        /**
+         * @brief send a response packet to the cpu_side port
+         * @param - packet to send
+         */
+        void sendRespPacket(PacketPtr pkt);
+
+        /**
+         * Send a retry to the peer port only if it is needed. This is called
+         * from the PuEngine whenever it is unblocked.
+         */
+        void trySendRetry();
+
+        /**
+         * @brief get address range list of this device owned this port
+         * @param void None
+         */
+        AddrRangeList getAddrRanges() const override;
+
+      protected:
+        /**
+         * @brief we do not implement AtomicRequest packet
+         *
+         * @param pkt
+         * @return Tick
+         */
+        Tick recvAtomic(PacketPtr pkt) override
+        { panic("recvAtomic Unimplemented"); }
+
+        /**
+         * @brief handle receved functional request packet from input ports
+         *
+         * @param pkt received
+         */
+        void recvFunctional(PacketPtr pkt) override;
+
+        /**
+         * @brief handle recved timing request packet from input ports
+         *
+         * @param pkt
+         */
+        bool recvTimingReq(PacketPtr pkt) override;
+
+        /**
+         * @brief notified from input ports that they are read to
+         *        accept resp packets
+         *
+         * @return true
+         * @return false
+         */
+        void recvRespRetry() override;
+
+    };
+
+    class MemSidePort : public RequestPort
+    {
+      private:
+        PuEngine *owner;
+
+        PacketPtr blockedPacket;
+
+      public:
+        MemSidePort(const std::string &name, PuEngine *owner) :
+          RequestPort(name, owner), blockedPacket(nullptr)
+        {}
+
+        /**
+         * @brief send a request packet pkt to devices
+         * connected to the memside_port
+         *
+         * @param pkt
+         */
+        void sendReqPacket(PacketPtr pkt);
+      protected:
+
+      /**
+       *
+       * @brief Receive a timing response from the response port
+       *         bool recvTimingResp(PacketPtr pkt) override;
+       * @param pkt
+       * @return true if successful
+       * @return false  if not successful (the peer should retry)
+       */
+        bool recvTimingResp(PacketPtr pkt) override;
+        /**
+         * @brief Notified by the peer that it is ready for
+         *        the req packet
+         *        Thus, this device can send the blocked request
+         */
+        void recvReqRetry() override;
+
+        /**
+         * @brief Notified that the peer changed its address range
+         *        Usually send this notification to the CPU side device
+         */
+        void recvRangeChange() override;
+
+    };
+
+private:
+
+    /// Instantiation of the CPU-side ports
+    CPUSidePort instPort;
+    CPUSidePort dataPort;
+
+    /// Instantiation of the memory-side port
+    MemSidePort memPort;
+
+    /// True if this is currently blocked waiting for a response.
+    bool blocked;
+
+    // PuCore - actually compute
+    PuCore * pucore;
     EventFunctionWrapper event;
-
-    /// Pointer to the corresponding GoodbyeObject. Set via Python
-    PuCore* pucore;
-
-    /// The name of this object in the Python config file
-    const std::string myName;
-
     /// Latency between calling the event (in ticks)
     const Tick latency;
-
     /// Number of times left to fire the event before pucore
     int timesLeft;
+
+  /**
+   * @brief actually handle a request packet from the CPU side
+   *
+   * @param pkt - Request Packet from the CPU side
+   * @return true - if successful
+   * @return false - negative
+   */
+    bool handleRequest(PacketPtr pkt);
+
+    /**
+     * @brief actually handle the response from the memory side objects
+     *
+     * @param pkt responding packet
+     * @return true - if successful
+     * @return false - if fail
+     */
+    bool handleResponse(PacketPtr pkt);
+
+    /**
+     * Handle a packet functionally. Update the data on a write and get the
+     * data on a read.
+     *
+     * @param packet to functionally handle
+     */
+    void handleFunctional(PacketPtr pkt);
+
+    /**
+     * Return the address ranges this memobj is responsible for. Just use the
+     * same as the next upper level of the hierarchy.
+     *
+     * @return the address ranges this memobj is responsible for
+     */
+    AddrRangeList getAddrRanges() const;
+
+    /**
+     * Tell the CPU side to ask for our memory ranges.
+     */
+    void sendRangeChange();
+
 
   public:
     PuEngine(const PuEngineParams &p);
@@ -70,6 +226,20 @@ class PuEngine : public SimObject
      * simulate() for the first time.
      */
     void startup();
+
+
+    void processEvent();
+
+    /**
+     * @brief Get the Port object
+     *
+     * @param if_name Port name
+     * @param idx   Index in case of VectorPort
+     * @return Port&
+     */
+    Port &getPort(const std::string &if_name,
+                  PortID idx=InvalidPortID) override;
+
 };
 
 } // namespace gem5
