7fabc3934f074658f92df28e95526b715d969ed5
soarstill <soarstill@gmail>
2022-08-09 00:38:36 +0900

    pudma: PuEngine.py
    Change-Id: I16864b7a6b49cc6c18ce8eee390f96374ee8eaeb

--- src/dev/pudma/PuEngine.py
+++ src/dev/pudma/PuEngine.py
@@ -28,13 +28,12 @@
 from m5.params import *
 from m5.SimObject import SimObject
 
-
 class PuCore(SimObject):
     type = 'PuCore'
     cxx_header = "dev/pudma/pu_core.hh"
     cxx_class = 'gem5::PuCore'
 
-    buffer_size = Param.MemorySize('1kB',
+    buffer_size = Param.MemorySize('100B',
                                    "Size of buffer to fill with goodbye")
     write_bandwidth = Param.MemoryBandwidth('100MB/s', "Bandwidth to fill "
                                             "the buffer")
@@ -48,3 +47,18 @@ class PuEngine(SimObject):
     number_of_fires = Param.Int(1, "Number of times to fire the event before "
                                    "goodbye")
     pucore_object = Param.PuCore("A PuCore object")
+
+    inst_port = ResponsePort("CPU side port, receve request from CPU side")
+    data_port = ResponsePort("CPU side port, receve request from CPU side")
+    mem_side = RequestPort("Memory side port, sends requests")
+
+class PuEngine2(SimObject):
+    type = 'PuEngine2'
+    cxx_header = "dev/pudma/pu_engine2.hh"
+    cxx_class = 'gem5::PuEngine2'
+
+    inst_port = ResponsePort("CPU side port, receives requests")
+    data_port = ResponsePort("CPU side port, receives requests")
+    mem_side = RequestPort("Memory side port, sends requests")
+
+    pucore = Param.PuCore("Actual compute core")
