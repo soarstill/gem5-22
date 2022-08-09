d21fa75e726af0496df9d10ea1a076aafa8f3631
soarstill <soarstill@gmail>
2022-08-09 15:21:49 +0900

    pudma: PuEngine.py
    Change-Id: I51c83398d34032d454bdf314de3c6107906008d2

--- src/dev/pudma/PuEngine.py
+++ src/dev/pudma/PuEngine.py
@@ -25,6 +25,7 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+from m5.objects.Device import BasicPioDevice
 from m5.params import *
 from m5.SimObject import SimObject
 
@@ -37,7 +38,6 @@ class PuCore(SimObject):
                                    "Size of buffer to fill with goodbye")
     write_bandwidth = Param.MemoryBandwidth('100MB/s', "Bandwidth to fill "
                                             "the buffer")
-
 class PuEngine(SimObject):
     type = 'PuEngine'
     cxx_header = "dev/pudma/pu_engine.hh"
@@ -62,3 +62,26 @@ class PuEngine2(SimObject):
     mem_side = RequestPort("Memory side port, sends requests")
 
     pucore = Param.PuCore("Actual compute core")
+
+class PuCore3(SimObject):
+    type = 'PuCore3'
+    cxx_header = "dev/pudma/pu_core3.hh"
+    cxx_class = 'gem5::PuCore3'
+
+    buffer_size = Param.MemorySize('100B',
+                                   "Size of buffer to fill with goodbye")
+    write_bandwidth = Param.MemoryBandwidth('100MB/s', "Bandwidth to fill "
+                                            "the buffer")
+
+class PuEngine3(BasicPioDevice): # IsaFake instead of BasicPioDevice??
+    type = 'PuEngine3'
+    cxx_header = "dev/pudma/pu_engine3.hh"
+    cxx_class = 'gem5::PuEngine3'
+
+    inst_port = ResponsePort("CPU side port, receives requests")
+    data_port = ResponsePort("CPU side port, receives requests")
+    mem_side = RequestPort("Memory side port, sends requests")
+
+    pio_addr = Param.Addr('0x300', "Pio Port start Address")
+    pio_size = Param.Addr('256B',"Pio Addr Size")
+    pucore3 = Param.PuCore3("Actual compute core")
