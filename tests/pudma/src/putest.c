cf3bf1e0d34885487173c0e4b6b2d5d9e15353af
soarstill <soarstill@gmail>
2022-08-09 21:09:20 +0900

    pudma: putest.c
    Change-Id: Ib1b025401fa9800f4197c86d723d257adcad2166

--- tests/pudma/src/putest.c
+++ tests/pudma/src/putest.c
@@ -37,40 +37,97 @@
 // lib headers
 #include <errno.h>
 #include <stdio.h>
+#include <string.h>
 #include <sys/io.h>
 
 typedef struct _DmaInfo
 {
-    unsigned int src;
-    unsigned int dst;
+    float * pa;
+    float * pb;
+    float * pc;
     unsigned int size;
     unsigned int cmd;
+    // cmd = 0: just send info, 1: start compute,
+    // 2: abort  3: report status
     unsigned int status;
+    // status 0: idle, 1: ready, 2: start-moving data,
+    // 3: in-computing 4: moving to mem 5: DONE
 } DmaInfo;
 
-volatile DmaInfo dmaInfo =
-    {0x01,0x02,0x03,0x04,0x05};
+DmaInfo dmaInfo ;
+DmaInfo dmaInfoRet ;
 
-unsigned long long pioAddr = 0x300;
-unsigned long long pioSize = 0x400;
+volatile const unsigned long long pioAddr = 0x300;
+volatile const unsigned long long pioSize = 0x400;
 
-void testPio()
+float dataA[1024];
+float dataB[1024];
+float dataC[1024];
+
+
+void printDmaInfo(DmaInfo * di)
 {
-
-    outsb(pioAddr, (void *)&dmaInfo, 16);
-    insb(pioAddr, ( void *)&dmaInfo, 16);
+    printf("-- DMA Info ---\n");
+    printf("  .pa = %#x \n", di->pa);
+    printf("  .pb = %#x \n", di->pb);
+    printf("  .pc = %#x \n", di->pc);
+    printf("  .size = %d \n", di->size);
+    printf("  .cmd = %d \n", di->cmd);
+    printf("  .status = %d \n\n", di->status);
 }
+
+int isEqual(DmaInfo *da, DmaInfo * db)
+{
+    if (da->pa != da->pa) return 0;
+    if (da->pb != da->pb) return 0;
+    if (da->pc != da->pc) return 0;
+    if (da->size != da->size) return 0;
+    if (da->cmd != da->cmd) return 0;
+    if (da->status != da->status) return 0;
+
+    printf("status: 0: idle, 1: ready, 2: start-moving data,\
+        3: in-computing 4: moving to mem 5: DONE\n");
+    printf("command:  0: just send info, 1: start compute, \
+        2: abort 3: report status\n");
+
+    return 1;
+}
+
+
+void testPio1()
+{
+    memset(&dmaInfo, 0, sizeof(dmaInfo));
+
+    dmaInfo.pa = &dataA[0];
+    dmaInfo.pb = &dataB[0];
+    dmaInfo.pc = &dataC[0];
+    dmaInfo.size = 1024;
+    dmaInfo.cmd = 0;
+    // 0: just send info, 1: start compute, 2: abort
+    // 3: report status
+    dmaInfo.status = 0;
+    // 0: idle, 1: ready, 2: start-moving data,
+    // 3: in-computing 4: moving to mem 5: DONE
+
+    printDmaInfo(&dmaInfo);
+    outsb(pioAddr, (void *)&dmaInfo, sizeof(dmaInfo));
+
+    memset(&dmaInfoRet, 0, sizeof(dmaInfo));
+    insb(pioAddr, (void *)&dmaInfoRet, sizeof(DmaInfo));
+    printDmaInfo(&dmaInfoRet);
+}
+
+
 
 int main(int argc, char* argv[])
 {
     printf("Hello world! - PU\n");
     printf("VA = %#x\n", &dmaInfo);
-    dmaInfo.src = 0x03; // WRITE REQ hook the v address in PuEngine2, 0x100
 
-    testPio();
+    testPio1();
 
     // printed 0x100 (NOT 0x03) (hooked by gem5), READ REQ hook by PuEngine2
-    printf("Hello world! - Done! Dma.src = %#x\n", dmaInfo.src);
+    printf("Hello world! - PU Done!\n\n");
     return 0;
 }
 
